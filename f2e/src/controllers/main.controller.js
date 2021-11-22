1bb9db64748f3b2296a26dc1e6d9156be5f87f15
Peter Huang <miterfrnats@gmail.com>
2021-11-23 03:19:35 +0800

    refactor: refine routing rule

--- f2e/src/controllers/main.controller.js
+++ f2e/src/controllers/main.controller.js
@@ -3,4 +3,11 @@ import {
 } from '../swim/routing-controller.js';
 
 export class MainController extends RoutingController {
+    async render () {
+        await super.render({
+            ...this.args.me,
+            numOfRegisteredUser: this.args.numOfRegisteredUser || 0,
+            user: this.args.me
+        });
+    }
 }

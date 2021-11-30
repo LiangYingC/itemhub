7a4fd485911cc7435994d39172a82ed7ee9232fe
Vicky <vickychou99@gmail.com>
2021-12-01 02:26:47 +0800

    feat: 優化文案

--- f2e/src/controllers/how.controller.js
+++ f2e/src/controllers/how.controller.js
@@ -12,9 +12,9 @@ export class HowController extends RoutingController {
         this.meta = {
             title: '如何使用 - ItemHub',
             'og:title': '如何使用 - ItemHub',
-            description: '簡單三步驟，即可完成連接與使用設定，連結你的裝置或感應器就是這麼輕鬆簡單，ItemHub 讓你專注在智慧裝置的運用，從今天開始你的智慧生活',
+            description: '簡單快速三步驟，即可完成連接與使用設定，串聯你的裝置或感應器就是這麼輕鬆簡單，ItemHub 讓你專注在智慧裝置的運用，從今天開始你的智慧生活',
             image: `${APP_CONFIG.FRONT_END_URL}/assets/images/share.png`,
-            keywords: 'ItemHub,item-hub,物聯網,iot,連結裝置,low-code,no-code,iot platform,iot,internet of thing,iot data center'
+            keywords: 'ItemHub,item-hub,物聯網,iot,串聯裝置,連結裝置,low-code,no-code,iot platform,iot,internet of thing,iot data center'
         };
         await super.render({
         });
--- f2e/src/controllers/main.controller.js
+++ f2e/src/controllers/main.controller.js
@@ -10,9 +10,9 @@ export class MainController extends RoutingController {
         this.meta = {
             title: 'ItemHub',
             'og:title': 'ItemHub',
-            description: '連接你的裝置與感應器就是這麼輕鬆簡單，ItemHub 讓你專注在智慧裝置的運用，從今天開始你的智慧生活',
+            description: '串聯你的裝置與感應器就是這麼輕鬆簡單，ItemHub 讓你專注在智慧裝置的運用，從今天開始你的智慧生活',
             image: `${APP_CONFIG.FRONT_END_URL}/assets/images/share.png`,
-            keywords: 'ItemHub,item-hub,物聯網,iot,連接裝置,low-code,no-code,iot platform,iot,internet of thing,iot data center'
+            keywords: 'ItemHub,item-hub,物聯網,iot,串聯裝置,連結裝置,low-code,no-code,iot platform,iot,internet of thing,iot data center'
         };
 
         await super.render({

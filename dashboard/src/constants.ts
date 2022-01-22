0eb1c53cfc44f8f7dd7210de2b8a1b0416aa5796
Peter Huang <miterfrnats@gmail.com>
2022-01-22 19:13:45 +0800

    feat: 加上 env variable

--- dashboard/src/constants.ts
+++ dashboard/src/constants.ts
@@ -4,7 +4,6 @@ export const RESPONSE_STATUS = {
 };
 
 export const END_POINT = {
-    API: 'https://localhost:5001/api/v1/',
     IS_SIGNED: 'auth/is-sign-in',
     DEVICES: 'me/devices',
     DEVICE: 'me/devices/:id',
--- dashboard/src/dataservices/device.dataservice.ts
+++ dashboard/src/dataservices/device.dataservice.ts
@@ -3,7 +3,9 @@ import { ApiHelper } from '../helpers/api.helper';
 
 export const DeviceDataservice = {
     GetList: async (token: string, page: number, limit: number) => {
-        const api = `${END_POINT.API}${END_POINT.DEVICES}?page=${page}&limit=${limit}`;
+        const api = `${import.meta.env.VITE_API_ENDPOINT}${
+            END_POINT.DEVICES
+        }?page=${page}&limit=${limit}`;
         const resp: any = await ApiHelper.SendRequestWithToken(
             api,
             {
@@ -16,7 +18,7 @@ export const DeviceDataservice = {
         return resp.data;
     },
     GetOne: async (token: string, id: number) => {
-        let api = `${END_POINT.API}${END_POINT.DEVICE}`;
+        let api = `${import.meta.env.VITE_API_ENDPOINT}${END_POINT.DEVICE}`;
 
         api = api.replace(':id', id.toString());
         const resp: any = await ApiHelper.SendRequestWithToken(

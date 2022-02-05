a4a440f1e3508e67afbf0e8e480cfb34c322eaf2
Liang C <sl20130313@gmail.com>
2022-02-06 03:58:46 +0800

    feat: implement temp dashboard simple login

--- dashboard/src/app.tsx
+++ dashboard/src/app.tsx
@@ -1,29 +1,54 @@
 import 'bootstrap/scss/bootstrap.scss';
 import './app.scss';
 import { Outlet } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { AuthDataservice } from './dataservices/auth.dataservice';
 import { CookieHelper } from './helpers/cookie.helper';
-// login.local.ts 不會進 Git，現階段請跟 coworker 拿
-import { DEV_LOGIN } from '@/login.local';
+
+const isProd = import.meta.env.VITE_ENV === 'prod';
 
 const App = () => {
-    // dev 環境暫時直接打測試帳號的 API 取得 token，便於開發，之後可拔掉
-    useEffect(() => {
-        const token = CookieHelper.GetCookie('token');
+    // dashboard dev site 暫時實作簡易的登入系統，之後 website 兩階段驗證完畢後可拔掉
+    const token = CookieHelper.GetCookie('token');
 
-        if (token === null && import.meta.env.VITE_ENV === 'dev') {
-            (async () => {
-                const data = await AuthDataservice.SignWithEmail({
-                    email: DEV_LOGIN.EMAIL,
-                    password: DEV_LOGIN.PASSWORD,
-                });
-                const tokenValue = data.token;
-                CookieHelper.SetCookie('token', tokenValue, 7);
-            })();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signWithEmail = async () => {
+        if (token === null && email && password && !isProd) {
+            const data = await AuthDataservice.SignWithEmail({
+                email,
+                password,
+            });
+            const tokenValue = data.token;
+            CookieHelper.SetCookie('token', tokenValue, 7);
+            window.location.reload();
         }
-    }, []);
-    return <Outlet />;
+    };
+
+    return token || isProd ? (
+        <Outlet />
+    ) : (
+        <>
+            <label>
+                <div>emial:</div>
+                <input
+                    type="email"
+                    name="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </label>
+            <label>
+                <div>password:</div>
+                <input
+                    type="password"
+                    name="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </label>
+            <button onClick={signWithEmail}>登入</button>
+        </>
+    );
 };
 
 export default App;

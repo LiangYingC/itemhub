91b0994b8bd6f5510aa7ac3f26a8a0ee3415943b
Liang C <sl20130313@gmail.com>
2022-02-06 17:37:04 +0800

    feat: implement refreshDevices, refreshSingleDevice and updateSingleDevice

--- dashboard/src/redux/reducers/devices.reducer.ts
+++ dashboard/src/redux/reducers/devices.reducer.ts
@@ -1,13 +1,53 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '@/redux/store';
-import { DeviceList } from '@/types/devices.type';
+import { DeviceList, DeviceItem } from '@/types/devices.type';
 
 export const devicesSlice = createSlice({
     name: 'devices',
-    initialState: null as DeviceList | null,
+    initialState: [] as DeviceList,
     reducers: {
-        setDevices: (state, action: PayloadAction<DeviceList>) => {
-            return action.payload;
+        refreshDevices: (state, action: PayloadAction<DeviceList>) => {
+            const newDevices = action.payload;
+            return newDevices;
+        },
+        refreshSingleDevice: (state, action: PayloadAction<DeviceItem>) => {
+            const devices = state;
+            const newDeviceData = action.payload;
+
+            const targetIndex = devices.findIndex(
+                (device) => device.id === newDeviceData.id
+            );
+            if (targetIndex < 0) {
+                devices.push(newDeviceData);
+            } else {
+                devices[targetIndex] = {
+                    ...newDeviceData,
+                };
+            }
+
+            return devices;
+        },
+        updateSingleDevice: (
+            state,
+            action: PayloadAction<Partial<DeviceItem>>
+        ) => {
+            const devices = state;
+            const newDeviceData = action.payload;
+
+            const targetIndex = devices.findIndex(
+                ({ id }) => id === newDeviceData.id
+            );
+            if (targetIndex < 0) {
+                throw new Error(
+                    'Can not find device which you want to update.'
+                );
+            }
+            devices[targetIndex] = {
+                ...devices[targetIndex],
+                ...newDeviceData,
+            };
+
+            return devices;
         },
     },
 });

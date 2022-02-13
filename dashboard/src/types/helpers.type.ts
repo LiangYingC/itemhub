828d60c493869dd7c3b3f6ef1a26d92d40c71ffc
Liang C <sl20130313@gmail.com>
2022-02-13 15:43:55 +0800

    feat：create ApiHelperInterface related type

--- dashboard/src/types/helpers.type.ts
+++ dashboard/src/types/helpers.type.ts
@@ -1,3 +1,59 @@
+interface SendRequestParams {
+    apiPath: string;
+    method: string;
+    headers?: { [key: string]: any };
+    payload?: any;
+    shouldDeleteContentType?: boolean;
+    callbackFunc?: (result: any) => null;
+}
+
+interface FetchParams {
+    apiPath: string;
+    fetchOption: {
+        method: string;
+        headers: { [key: string]: any };
+        body?: string;
+    };
+    shouldDeleteContentType: boolean;
+}
+
+export interface FetchResult {
+    httpStatus: number;
+    status: string;
+    data: { [key: string]: any };
+}
+
+export interface ApiHelperInterface {
+    sendRequestWithToken: ({
+        apiPath,
+        method,
+        headers,
+        payload,
+        shouldDeleteContentType,
+        callbackFunc,
+    }: SendRequestParams) => Promise<FetchResult>;
+    sendRequest: ({
+        apiPath,
+        method,
+        headers,
+        payload,
+        shouldDeleteContentType,
+        callbackFunc,
+    }: SendRequestParams) => Promise<FetchResult>;
+    fetch: ({
+        apiPath,
+        fetchOption,
+        shouldDeleteContentType,
+    }: FetchParams) => Promise<Response>;
+    handleDownloadFile: ({ response }: { response: Response }) => Promise<{
+        httpStatus: number;
+        status: string;
+        data: {
+            message: string;
+        };
+    }>;
+}
+
 export interface CookieHelperInterface {
     setCookie: ({
         name,

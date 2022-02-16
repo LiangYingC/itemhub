ce1a8609ac03a492c745d5c502259782c7b8ed5d
Peter Huang <miterfrnats@gmail.com>
2022-02-17 00:26:47 +0800

    feat: sign up with social media

--- website/src/controllers/master.controller.js
+++ website/src/controllers/master.controller.js
@@ -1,10 +1,14 @@
-// import { RESPONSE_STATUS } from '../constants.js';
-// import { UserDataService } from '../dataservices/user.dataservice.js';
 import {
     RoutingController
 } from '../swim/routing-controller.js';
+import { Toaster } from '../util/toaster.js';
 
 export class MasterController extends RoutingController {
+    constructor (elHTML, parentController, args, context) {
+        super(elHTML, parentController, args, context);
+        this.Toaster = Toaster;
+    }
+
     static get id () {
         return 'MasterController';
     }
--- website/src/controllers/oauth.controller.js
+++ website/src/controllers/oauth.controller.js
@@ -4,7 +4,6 @@ import {
 import { EVENTS, OAUTH_TYPE, RESPONSE_STATUS } from '../constants.js';
 import { AuthDataService } from '../dataservices/auth.dataservice.js';
 import { Toaster } from '../util/toaster.js';
-import { Swissknife } from '../util/swissknife.js';
 import { CookieUtil } from '../util/cookie.js';
 
 export class OauthController extends RoutingController {
@@ -16,8 +15,8 @@ export class OauthController extends RoutingController {
         await super.render({
             provider: this.args.state.provider
         });
-        if (this.args.state.type === OAUTH_TYPE.SIGN_UP) {
-            await this.signUp();
+        if (this.args.state.type === OAUTH_TYPE.VERIFY_EMAIL_WITH_SOCIAL_MEDIA) {
+            await this.getVerifyPhoneToken();
         } else if (this.args.state.type === OAUTH_TYPE.SIGN_IN) {
             await this.signIn();
         }
@@ -44,36 +43,21 @@ export class OauthController extends RoutingController {
         window.close();
     }
 
-    async signUp () {
-        const targetProvider = this.args.universal.authProvider.find(item => item.label.toString().toLowerCase() === this.args.state.provider);
-        const resp = await AuthDataService.AuthWithSocialMedia({
+    async getVerifyPhoneToken () {
+        const targetProvider = this.args.socialMediaTypes.find(item => item.label.toString().toLowerCase() === this.args.state.provider);
+        const resp = await AuthDataService.VerifyEmailWithSocialMedia({
             code: this.args.code,
             provider: targetProvider.value,
             redirectUri: `${location.origin}${location.pathname}`
         });
 
         if (resp.status !== RESPONSE_STATUS.OK) {
-            Toaster.popup(Toaster.TYPE.ERROR, resp.data.message);
-            setTimeout(() => {
-                window.close();
-            }, 5000);
+            opener.SwimAppController[1].Toaster.popup(Toaster.TYPE.ERROR, resp.data.message);
+            window.close();
             return;
         }
 
-        const respOfSignUp = await AuthDataService.SignUp({
-            token: resp.data.token,
-            password: Swissknife.GenerateRandomText(36) // 為了讓 sign-up api 參數保持一至, 這邊產生亂數密碼用 oAuth Provider 登入用不到
-        });
-        if (respOfSignUp.status !== RESPONSE_STATUS.OK) {
-            Toaster.popup(Toaster.TYPE.ERROR, respOfSignUp.data.message);
-            setTimeout(() => {
-                window.close();
-            }, 5000);
-            return;
-        }
-        opener.gtag('event', EVENTS.SIGN_UP);
-        CookieUtil.setCookie('token', respOfSignUp.data.token);
-        opener.history.pushState({}, '', '/');
+        opener.history.pushState({}, '', `/auth/sign-up/?verifyPhoneToken=${resp.data.token}`);
         window.close();
     }
 }

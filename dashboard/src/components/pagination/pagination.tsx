a46ccd46ef134dcf89119f0c8bf9ae7090d52821
Peter Huang <miterfrnats@gmail.com>
2022-02-22 16:53:26 +0800

    refactor: add pagination props type

--- dashboard/src/components/pagination/pagination.tsx
+++ dashboard/src/components/pagination/pagination.tsx
@@ -2,17 +2,21 @@ import styles from './pagination.module.scss';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-const Pagination = (props: any | undefined) => {
+const Pagination = (props: {
+    rowNums: number;
+    limit: number;
+    page: number;
+}) => {
     const [pages, setPages] = useState<Array<number>>([]);
     const { rowNums, limit, page } = props;
 
     useEffect(() => {
         const maxPage = Math.ceil(rowNums / limit);
-        const _pages = [];
+        const pages = [];
         for (let i = 1; i <= maxPage; i++) {
-            _pages.push(i);
+            pages.push(i);
         }
-        setPages(_pages);
+        setPages(pages);
     }, [rowNums, limit]);
 
     return (

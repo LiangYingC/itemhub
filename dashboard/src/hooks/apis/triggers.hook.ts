991b8bca922e0c793d8f45b981aee0a5ff95fa63
Liang C <sl20130313@gmail.com>
2022-02-28 21:17:38 +0800

    feat: implement useGetTriggerApi

--- dashboard/src/hooks/apis/triggers.hook.ts
+++ dashboard/src/hooks/apis/triggers.hook.ts
@@ -50,3 +50,59 @@ export const useGetTriggersApi = ({
         getTriggersApi: fetchApi,
     };
 };
+
+export const useGetTriggerApi = (id: number) => {
+    const fetchGetTriggerMethod = useCallback(async () => {
+        let apiPath = `${API_URL}${END_POINT.TRIGGER}`;
+        apiPath = apiPath.replace(':id', id.toString());
+
+        // const result = await ApiHelpers.SendRequestWithToken<TriggerItem>({
+        //     apiPath,
+        //     method: HTTP_METHOD.GET,
+        // });
+
+        /**
+         * 目前 API 尚未實作完成，先吐假資料，待接下來實作 API 後，
+         * 再把打開上面打 API 的程式、下面 return 拔掉，直接回傳 “result.data” 即可。
+         * */
+        return {
+            createdAt: '2022-01-09T22:44:58',
+            deletedAt: null,
+            destinationDevice: {},
+            destinationDeviceId: 60,
+            destinationDeviceSourceState: 0,
+            destinationDeviceTargetState: 1,
+            destinationPin: 'D3',
+            editedAt: null,
+            id: 2,
+            operator: 0,
+            ownerId: 23,
+            sourceDevice: {},
+            sourceDeviceId: 60,
+            sourcePin: 'D2',
+            sourceThreshold: 62,
+        } as TriggerItem;
+    }, [id]);
+
+    const dispatch = useAppDispatch();
+    const dispatchRefreshTrigger = useCallback(
+        (data: TriggerItem) => {
+            if (data) {
+                dispatch(triggersActions.refreshTrigger(data));
+            }
+        },
+        [dispatch]
+    );
+
+    const { isLoading, error, fetchApi } = useFetchApi<TriggerItem>({
+        initialData: null,
+        fetchMethod: fetchGetTriggerMethod,
+        callbackFunc: dispatchRefreshTrigger,
+    });
+
+    return {
+        isGettingTrigger: isLoading,
+        getTriggerError: error,
+        getTriggerApi: fetchApi,
+    };
+};

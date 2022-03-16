97fa38ec7caba30ed246b79748b2b8e45ba19945
Peter Huang <miterfrnats@gmail.com>
2022-03-16 17:13:00 +0800

    feat: 加上 oAuthClient.ClientId  & DeletedAt 要是 Unique

---
+++ api/Migrations/20220316083947_ChangeOauthClientUniqueIndex3.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IotApi.Migrations
+{
+    public partial class ChangeOauthClientUniqueIndex3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_OauthClient_ClientId_DeletedAt",
+                table: "OauthClient",
+                columns: new[] { "ClientId", "DeletedAt" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OauthClient_ClientId_DeletedAt",
+                table: "OauthClient");
+        }
+    }
+}

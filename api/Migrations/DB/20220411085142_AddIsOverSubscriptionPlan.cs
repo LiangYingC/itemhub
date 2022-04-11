0cb5ea6cfb2494d26eb6f67dd06b8a6aa42baba1
Peter Huang <miterfrnats@gmail.com>
2022-04-11 17:02:54 +0800

    feat: db migration

---
+++ api/Migrations/DB/20220411085142_AddIsOverSubscriptionPlan.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IotApi.Migrations.DB
+{
+    public partial class AddIsOverSubscriptionPlan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOverSubscriptionPlan",
+                table: "User",
+                type: "tinyint(1)",
+                nullable: true,
+                defaultValueSql: "0");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOverSubscriptionPlan",
+                table: "User");
+        }
+    }
+}

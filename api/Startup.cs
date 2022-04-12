de8ec910700127142d9b30fce782d541274a12ef
Peter Huang <miterfrnats@gmail.com>
2022-04-12 15:08:43 +0800

    feat: 把 auto payment 的 cronjob 執行時間變成每天凌晨

--- api/Startup.cs
+++ api/Startup.cs
@@ -81,7 +81,7 @@ namespace Homo.IotApi
                 services.AddCronJob<AutoPaymentCronJob>(c =>
                 {
                     c.TimeZoneInfo = TimeZoneInfo.Local;
-                    c.CronExpression = @"* * * * *";
+                    c.CronExpression = @"0 0 * * *";
                 });
             }
 

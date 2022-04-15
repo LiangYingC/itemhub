8a1c5418ca38762284fce979698aac1d4b68f7a7
Liang C <sl20130313@gmail.com>
2022-04-15 23:43:02 +0800

    feat: get universal constant triggerOperators on app.tsx

--- dashboard/src/app.tsx
+++ dashboard/src/app.tsx
@@ -1,27 +1,36 @@
 import 'bootstrap/scss/bootstrap.scss';
 import './app.scss';
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useQuery } from './hooks/query.hook';
+import { useGetTriggerOperatorsApi } from '@/hooks/apis/universal.hook';
 import { CookieHelpers } from './helpers/cookie.helper';
+import { COOKIE_KEY } from './constants/cookie-key';
 
 const isDev = import.meta.env.VITE_ENV === 'dev';
-import { useQuery } from './hooks/query.hook';
-import { COOKIE_KEY } from './constants/cookie-key';
 
 const App = () => {
     const query = useQuery();
     const dashboardTokenFromQueryString =
         query.get(COOKIE_KEY.DASHBOARD_TOKEN) || '';
+
     if (isDev && dashboardTokenFromQueryString) {
         CookieHelpers.SetCookie({
             name: COOKIE_KEY.DASHBOARD_TOKEN,
             value: dashboardTokenFromQueryString,
         });
     }
+
     const token = CookieHelpers.GetCookie({ name: COOKIE_KEY.DASHBOARD_TOKEN });
     if (!token) {
         location.href = import.meta.env.VITE_WEBSITE_URL;
     }
-    return token ? <Outlet /> : <>Redirect</>;
+
+    const { getTriggerOperatorsApi } = useGetTriggerOperatorsApi();
+    useEffect(() => {
+        getTriggerOperatorsApi();
+    }, []);
+    return token ? <Outlet /> : <>Redirect To Login</>;
 };
 
 export default App;

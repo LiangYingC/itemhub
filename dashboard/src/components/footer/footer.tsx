2be0c5c692fec8ac306af655d00175e308226f32
Vicky <vickychou99@gmail.com>
2022-04-16 02:49:07 +0800

    feat: 更新 footer 連結

--- dashboard/src/components/footer/footer.tsx
+++ dashboard/src/components/footer/footer.tsx
@@ -1,13 +1,25 @@
+import { Link } from 'react-router-dom';
+
 const Footer = () => (
     <div
-        className="footer d-flex justify-content-between p-5"
+        className="footer d-flex justify-content-between p-5 h6"
         data-testid="Footer"
     >
         <div className="flex-fill">
-            <span>關於我們</span>
-            <span className="ps-5">隱私權政策</span>
+            <Link
+                to="/dashboard/"
+                className="text-decoration-none text-black text-opacity-65"
+            >
+                關於我們
+            </Link>
+            <Link
+                to="/dashboard/"
+                className="text-decoration-none text-black text-opacity-65 ps-5"
+            >
+                隱私權政策
+            </Link>
         </div>
-        <div className="flex-fill text-end">
+        <div className="flex-fill text-end text-black text-opacity-65">
             Copyright © 2021 itemhub Inc. All rights reserved.
         </div>
     </div>

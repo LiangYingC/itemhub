9fa869f6ca9407dc7aa378f3bbe0077950dbcaad
Liang C <sl20130313@gmail.com>
2022-04-17 12:58:15 +0800

    feat: implement DeviceAndPinInputs component

---
+++ dashboard/src/components/Inputs/deviceAndPinInput/deviceAndPinInput.tsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import { DeviceItem, PinItem } from '@/types/devices.type';
+import AutocompletedSearch from '@/components/autocompletedSearch/autocompletedSearch';
+
+const DeviceAndPinInputs = ({
+    allDevices,
+    initialDeviceName = '',
+    deviceIdLable,
+    deviceIdValue,
+    deviceNameLabel,
+    pinLabel,
+    pinValue,
+    pinOptions,
+    updatePin,
+    updateDeviceId,
+}: {
+    allDevices: DeviceItem[];
+    initialDeviceName?: string;
+    deviceIdLable: string;
+    deviceIdValue: number;
+    deviceNameLabel: string;
+    pinLabel: string;
+    pinValue: string;
+    pinOptions: PinItem[];
+    updatePin: (pin: string) => void;
+    updateDeviceId: (id: number) => void;
+}) => {
+    const [deviceName, setDeviceName] = useState(initialDeviceName);
+
+    const currentDeviceId =
+        allDevices.filter(({ name }) => deviceName === name)[0]?.id || null;
+
+    useEffect(() => {
+        if (currentDeviceId) {
+            updateDeviceId(currentDeviceId);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentDeviceId]);
+
+    return (
+        <>
+            <div className="form-group mt-3">
+                <label>{deviceIdLable}</label>
+                <input
+                    className="form-control"
+                    disabled
+                    value={deviceIdValue}
+                />
+            </div>
+            <div className="form-group mt-3">
+                <label>{deviceNameLabel}</label>
+                <AutocompletedSearch
+                    currentValue={deviceName}
+                    updateCurrentValue={(newValue) => setDeviceName(newValue)}
+                    allSuggestions={allDevices.map(({ name }) => name)}
+                />
+            </div>
+            <div className="form-group mt-3">
+                <label>{pinLabel}</label>
+                <select
+                    value={pinValue}
+                    onChange={(e) => {
+                        const newSourcePin = e.target.value;
+                        updatePin(newSourcePin);
+                    }}
+                >
+                    {pinOptions.map(({ name, pin }, index) => {
+                        return (
+                            <option key={`${name}-${index}`} value={pin}>
+                                {name}
+                            </option>
+                        );
+                    })}
+                </select>
+            </div>
+        </>
+    );
+};
+
+export default DeviceAndPinInputs;

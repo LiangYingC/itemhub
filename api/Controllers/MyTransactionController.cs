ca2db151ea1f69381ba3f3d77d5132f07f5d7a13
Vicky <vickychou99@gmail.com>
2022-04-18 23:58:28 +0800

    feat: 把把信程式移到切換付款狀態的地方

--- api/Controllers/MyTransactionController.cs
+++ api/Controllers/MyTransactionController.cs
@@ -2,12 +2,8 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Homo.Api;
-using Homo.AuthApi;
 using Homo.Core.Constants;
 using System.Linq;
-using System.Threading.Tasks;
-using Homo.Core.Helpers;
-
 
 namespace Homo.IotApi
 {
@@ -17,19 +13,12 @@ namespace Homo.IotApi
     [Validate]
     public class MyTransactionController : ControllerBase
     {
-        private readonly Homo.Api.CommonLocalizer _commonLocalizer;
         private readonly IotDbContext _dbContext;
         private readonly string _dbConnectionString;
-        private readonly string _systemEmail;
-        private readonly string _adminEmail;
-        private readonly string _sendGridApiKey;
-        public MyTransactionController(IotDbContext dbContext, IOptions<AppSettings> appSettings, Homo.Api.CommonLocalizer commonLocalizer)
+
+        public MyTransactionController(IotDbContext dbContext, IOptions<AppSettings> appSettings)
         {
-            _commonLocalizer = commonLocalizer;
             _dbContext = dbContext;
-            _systemEmail = appSettings.Value.Common.SystemEmail;
-            _adminEmail = appSettings.Value.Common.AdminEmail;
-            _sendGridApiKey = appSettings.Value.Secrets.SendGridApiKey;
         }
 
         [HttpGet]
@@ -37,18 +26,6 @@ namespace Homo.IotApi
         {
             long ownerId = extraPayload.Id;
             Transaction result = TransactionDataservice.GetOne(_dbContext, ownerId, id);
-            // tappay 回傳確認付款成功後，發信給管理員
-            // 主要是其中一個管理員看到有付費會員會覺得開心，想要自爽用的
-            if (result.Status == TRANSACTION_STATUS.PAID)
-            {
-                MailHelper.Send(MailProvider.SEND_GRID, new MailTemplate()
-                {
-                    Subject = _commonLocalizer.Get("get new premium user"),
-                    Content = _commonLocalizer.Get("premium user id", null, new Dictionary<string, string>() {
-                    { "userId", $"{extraPayload.Id}" }
-                })
-                }, _systemEmail, _adminEmail, _sendGridApiKey);
-            }
 
             return result;
         }

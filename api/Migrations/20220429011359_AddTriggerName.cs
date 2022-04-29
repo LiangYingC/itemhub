68b056b6f1c2997713e96aa818d10176f199f884
Peter Huang <miterfrnats@gmail.com>
2022-04-29 09:26:05 +0800

    feat: migrate trigger name field

---
+++ api/Migrations/20220429011359_AddTriggerName.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IotApi.Migrations
+{
+    public partial class AddTriggerName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Trigger",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trigger_Name",
+                table: "Trigger",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Trigger_Name",
+                table: "Trigger");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Trigger");
+        }
+    }
+}

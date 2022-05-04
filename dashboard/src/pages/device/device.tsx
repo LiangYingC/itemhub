8eb7a5cceb146c9ea1231e3fdabd5b65135829c4
Vicky <vickychou99@gmail.com>
2022-05-04 22:34:00 +0800

    feat: 實作顯示 device 內頁資訊及刪除 device

--- dashboard/src/pages/device/device.tsx
+++ dashboard/src/pages/device/device.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import {
     useDeleteDevicesApi,
     useGetDeviceApi,
@@ -10,61 +10,49 @@ import { selectDevices } from '@/redux/reducers/devices.reducer';
 import Pins from '@/components/pins/pins';
 import { RESPONSE_STATUS } from '@/constants/api';
 import PageTitle from '@/components/page-title/page-title';
-import { useGetOauthClientByDeviceId } from '@/hooks/apis/oauth-clients.hook';
-import { selectOauthClients } from '@/redux/reducers/oauth-clients.reducer';
 import { useDispatch } from 'react-redux';
 import { dialogActions, DialogTypeEnum } from '@/redux/reducers/dialog.reducer';
+import trashIcon from '@/assets/images/trash.svg';
+import {
+    toasterActions,
+    ToasterTypeEnum,
+} from '@/redux/reducers/toaster.reducer';
+import moment from 'moment';
 
 const Device = () => {
-    const { id } = useParams();
-    const numId = Number(id);
+    const { id: idFromUrl } = useParams();
+    const id: number | null = idFromUrl ? Number(idFromUrl) : null;
+
     const devices = useAppSelector(selectDevices).devices;
-    const oAuthClients = useAppSelector(selectOauthClients).oauthClients;
+
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const device =
-        (devices || []).filter((device) => device.id === numId)[0] || null;
-
-    const oAuthClient =
-        oAuthClients?.filter((client) => client.deviceId === device?.id)[0] ||
-        null;
+        (devices || []).filter((device) => device.id === Number(id))[0] || null;
 
     const [deviceName, setDeviceName] = useState<string>('');
 
-    const { isLoading, getDeviceApi } = useGetDeviceApi({
-        id: numId,
-    });
-
-    const {
-        isLoading: isOauthClientLoading,
-        fetchApi: getOauthClientByDeviceId,
-    } = useGetOauthClientByDeviceId(device ? device.id : 0);
+    const { isLoading: isGetting, fetchApi: getDeviceApi } = useGetDeviceApi(
+        Number(id)
+    );
 
     const { updateDeviceApi, isLoading: isUpdating } = useUpdateDeviceApi({
-        id: numId,
+        id: Number(id),
         editedData: {
             name: deviceName,
             deviceId: device?.deviceId,
         },
     });
 
-    const {
-        fetchApi: deleteMultipleApi,
-        isLoading: isDeleting,
-        data: deleteDeviceResponse,
-    } = useDeleteDevicesApi([numId]);
+    const { fetchApi: deleteApi, data: deleteDeviceResponse } =
+        useDeleteDevicesApi([Number(id)]);
 
-    const deleteDevice = () => {
-        dispatch(
-            dialogActions.open({
-                message: `刪除後將無法復原, 請輸入 DELETE 完成刪除 ${deviceName}`,
-                title: '確認刪除裝置 ?',
-                type: DialogTypeEnum.PROMPT,
-                checkedMessage: 'DELETE',
-                callback: deleteMultipleApi,
-                promptInvalidMessage: '輸入錯誤',
-            })
-        );
+    const backToList = () => {
+        navigate('/dashboard/devices');
+    };
+
+    const jumpToEditPage = () => {
+        navigate('edit');
     };
 
     useEffect(() => {
@@ -74,114 +62,103 @@ const Device = () => {
     }, []);
 
     useEffect(() => {
-        if (device === null) {
-            return;
-        }
-        if (oAuthClient === null) {
-            getOauthClientByDeviceId();
-        }
-    }, [device]);
-
-    useEffect(() => {
         setDeviceName(device ? device.name : '');
     }, [device]);
 
     useEffect(() => {
         if (deleteDeviceResponse?.status === RESPONSE_STATUS.OK) {
-            navigate('../devices', { replace: true });
+            dispatch(
+                toasterActions.pushOne({
+                    message: `已經成功刪除 ${deviceName} `,
+                    duration: 5,
+                    type: ToasterTypeEnum.INFO,
+                })
+            );
+            navigate('/dashboard/devices', { replace: true });
         }
-    }, [navigate, deleteDeviceResponse]);
+    }, [deleteDeviceResponse]);
+
+    const deleteDevice = () => {
+        dispatch(
+            dialogActions.open({
+                message: `刪除後將無法復原, 請輸入 DELETE 完成刪除 ${deviceName} `,
+                title: '確認刪除裝置 ?',
+                type: DialogTypeEnum.PROMPT,
+                checkedMessage: 'DELETE',
+                callback: deleteApi,
+                promptInvalidMessage: '輸入錯誤',
+            })
+        );
+    };
 
     return (
         // UI 結構等設計稿後再重構調整
         <div className="device" data-testid="device">
-            <PageTitle title={`裝置 - ${deviceName}`} />
-            {isLoading || device === null ? (
+            <PageTitle
+                titleClickCallback={backToList}
+                titleBackIconVisible
+                title={`${deviceName}詳細內容`}
+                primaryButtonVisible
+                primaryButtonWording="編輯"
+                primaryButtonCallback={jumpToEditPage}
+                secondaryButtonIcon={trashIcon}
+                secondaryButtonVisible
+                secondaryButtonWording="刪除"
+                secondaryButtonCallback={deleteDevice}
+            />
+            {isGetting || device === null ? (
                 <div>Loading</div>
             ) : (
-                <div>
-                    <div className="mb-4">
-                        <div className="form-group mt-3">
-                            <label>裝置名稱</label>
-                            <input
-                                className="form-control"
-                                value={deviceName}
-                                onChange={(e) => setDeviceName(e.target.value)}
-                            />
+                <div className="card">
+                    <div className="row m-0">
+                        <div className="col-12 col-md-6 d-flex p-0 item">
+                            <div className="d-flex flex-shrink-0 fs-5 item-title py-2 px-25">
+                                Device Id
+                            </div>
+                            <div className="text-wrap text-black text-opacity-65 py-2 px-25">
+                                {device.deviceId}
+                            </div>
                         </div>
-
-                        <div className="form-group mt-3">
-                            <label>裝置 ID</label>
-                            <input
-                                className="form-control"
-                                disabled
-                                value={device.deviceId}
-                            />
+                        <div className="col-12 col-md-6 d-flex p-0 item">
+                            <div className="d-flex flex-shrink-0 fs-5 item-title py-2 px-25">
+                                狀態
+                            </div>
+                            <div className="py-2 px-25">
+                                <div
+                                    className={`tag fs-5 ${
+                                        device.online ? 'tag-green' : 'tag-grey'
+                                    }`}
+                                >
+                                    <div>{device.online ? '上線' : '離線'}</div>
+                                </div>
+                            </div>
                         </div>
-
-                        <div className="form-group mt-3">
-                            <label>建立時間</label>
-                            <input
-                                className="form-control"
-                                disabled
-                                value={device.createdAt}
-                            />
+                        <div className="col-12 col-md-6 d-flex p-0 item">
+                            <div className="d-flex flex-shrink-0 fs-5 item-title py-2 px-25">
+                                建立時間
+                            </div>
+                            <div className="text-wrap text-black text-opacity-65 py-2 px-25">
+                                {` ${moment(device.createdAt).format(
+                                    'YYYY-MM-DD HH:mm'
+                                )}`}
+                            </div>
                         </div>
-
-                        <div className="form-group mt-3">
-                            <label>狀態</label>
-                            <input
-                                className="form-control"
-                                disabled
-                                value={device.online ? '開' : '關'}
-                            />
+                        <div className="col-12 p-0 item">
+                            <div className="col-12 col-md-6 d-flex p-0">
+                                <div className="d-flex flex-shrink-0 fs-5 item-title py-2 px-25">
+                                    Pins Data
+                                </div>
+                                <div className="text-wrap text-black text-opacity-65 py-2 px-25">
+                                    <Pins
+                                        deviceId={Number(id)}
+                                        isEditMode={false}
+                                    />
+                                </div>
+                            </div>
                         </div>
-
-                        <button
-                            className="btn border mt-3"
-                            onClick={updateDeviceApi}
-                            disabled={isUpdating}
-                        >
-                            {isUpdating ? '更新中' : '更新'}
-                        </button>
-
-                        <button
-                            className="btn border mt-3 ms-3"
-                            onClick={deleteDevice}
-                            disabled={isDeleting}
-                        >
-                            {isUpdating ? '刪除中' : '刪除'}
-                        </button>
-                    </div>
-                    <div>
-                        <h2>Pins Data</h2>
-                        <Pins deviceId={Number(id)} isEditMode />
                     </div>
                 </div>
             )}
-
-            {isOauthClientLoading ? (
-                <div>Loading</div>
-            ) : (
-                <>
-                    <div>
-                        <label>ClientId: </label>
-                        <input
-                            className="form-control"
-                            value={oAuthClient?.clientId}
-                        />
-                    </div>
-                    <div>
-                        <label>Secret: </label>
-                        <div className="form-control">
-                            {oAuthClient?.clientSecrets || '*****'}
-                        </div>
-                    </div>
-                </>
-            )}
-            <div>
-                <Link to="../dashboard/devices">Back to device list</Link>
-            </div>
         </div>
     );
 };

6bedbe1fd347ccae2762d946c72f9e8c496265b3
Liang C <sl20130313@gmail.com>
2022-05-16 21:51:48 +0800

    feat: implement trigger read mode table

--- dashboard/src/pages/trigger/trigger.tsx
+++ dashboard/src/pages/trigger/trigger.tsx
@@ -5,23 +5,27 @@ import {
     useGetTriggerApi,
     useDeleteTriggersApi,
 } from '@/hooks/apis/triggers.hook';
+import { Link } from 'react-router-dom';
 import { RESPONSE_STATUS } from '@/constants/api';
 import { dialogActions, DialogTypeEnum } from '@/redux/reducers/dialog.reducer';
 import {
     toasterActions,
     ToasterTypeEnum,
 } from '@/redux/reducers/toaster.reducer';
+import { selectUniversal } from '@/redux/reducers/universal.reducer';
 import { selectTriggers } from '@/redux/reducers/triggers.reducer';
 import { TriggerItem } from '@/types/triggers.type';
 import TriggerForm from './trigger-form/trigger-form';
 import PageTitle from '@/components/page-title/page-title';
 import Spinner from '@/components/spinner/spinner';
+import OnlineStatusTag from '@/components/online-status-tag/online-status-tag';
 import trashIcon from '@/assets/images/trash.svg';
 
 const Trigger = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
+    const { triggerOperators } = useAppSelector(selectUniversal);
 
     const { id: idFromUrl } = useParams();
     const triggerId = idFromUrl ? parseInt(idFromUrl) : null;
@@ -93,27 +97,95 @@ const Trigger = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [deleteTriggersResponse, dispatch]);
 
+    const isLoading = isGettingTrigger || (trigger === null && !isCreateMode);
+    const isReadMode = !isCreateMode && !isEditMode && trigger !== null;
+
     return (
         <div className="trigger" data-testid="trigger">
-            {isGettingTrigger || (trigger === null && !isCreateMode) ? (
+            {isLoading ? (
                 <div className="w-100 d-flex justify-content-center mt-5">
                     <Spinner />
                 </div>
-            ) : isCreateMode || isEditMode ? (
-                <TriggerForm trigger={trigger} isCreateMode={isCreateMode} />
+            ) : isReadMode ? (
+                <>
+                    <PageTitle
+                        titleClickCallback={jumpToListPage}
+                        titleBackIconVisible
+                        title={triggerName}
+                        primaryButtonVisible
+                        primaryButtonWording="編輯"
+                        primaryButtonCallback={jumpToEditPage}
+                        secondaryButtonVisible
+                        secondaryButtonIcon={trashIcon}
+                        secondaryButtonWording="刪除"
+                        secondaryButtonCallback={confirmToDeleteTrigger}
+                    />
+                    <div className="card">
+                        <div className="row m-0 border border-white">
+                            <div className="col-4 col-md-2 col-lg-2 py-2 bg-black bg-opacity-5 text-black text-opacity-45">
+                                事件
+                            </div>
+                            <div className="col-8 col-md-10 col-lg-4 py-2">
+                                <div className="d-flex">
+                                    <div className="me-2">
+                                        <Link
+                                            to={`/dashboard/devices/${trigger.sourceDeviceId}`}
+                                        >
+                                            {trigger.sourceDevice?.name}
+                                        </Link>
+                                    </div>
+                                    <OnlineStatusTag
+                                        isOnline={
+                                            trigger.sourceDevice?.online ||
+                                            false
+                                        }
+                                    />
+                                </div>
+                                <div>Pin: {trigger.sourcePin}</div>
+                            </div>
+                            <div className="col-4 col-md-2 col-lg-2 py-2 bg-black bg-opacity-5 text-black text-opacity-45">
+                                條件
+                            </div>
+                            <div className="col-8 col-md-10 col-lg-4 py-2">
+                                {triggerOperators[trigger.operator]?.label}{' '}
+                                {trigger.sourceThreshold}
+                            </div>
+                        </div>
+                        <div className="row m-0 border border-white">
+                            <div className="col-4 col-md-2 col-lg-2 py-2 bg-black bg-opacity-5 text-black text-opacity-45">
+                                目標
+                            </div>
+                            <div className="col-8 col-md-10 col-lg-4 py-2">
+                                <div className="d-flex">
+                                    <div className="me-2">
+                                        <Link
+                                            to={`/dashboard/devices/${trigger.destinationDeviceId}`}
+                                        >
+                                            {trigger.destinationDevice?.name}
+                                        </Link>
+                                    </div>
+                                    <OnlineStatusTag
+                                        isOnline={
+                                            trigger.destinationDevice?.online ||
+                                            false
+                                        }
+                                    />
+                                </div>
+                                <div>Pin: {trigger.destinationPin}</div>
+                            </div>
+                            <div className="col-4 col-md-2 col-lg-2 py-2 bg-black bg-opacity-5 text-black text-opacity-45">
+                                目標狀態
+                            </div>
+                            <div className="col-8 col-md-10 col-lg-4 py-2">
+                                {trigger.destinationDeviceTargetState === 1
+                                    ? '開'
+                                    : '關'}
+                            </div>
+                        </div>
+                    </div>
+                </>
             ) : (
-                <PageTitle
-                    titleClickCallback={jumpToListPage}
-                    titleBackIconVisible
-                    title={triggerName}
-                    primaryButtonVisible
-                    primaryButtonWording="編輯"
-                    primaryButtonCallback={jumpToEditPage}
-                    secondaryButtonVisible
-                    secondaryButtonIcon={trashIcon}
-                    secondaryButtonWording="刪除"
-                    secondaryButtonCallback={confirmToDeleteTrigger}
-                />
+                <TriggerForm trigger={trigger} isCreateMode={isCreateMode} />
             )}
         </div>
     );

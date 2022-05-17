08324269efb323bcdf692b65babc29e7d1e5fb99
Peter Huang <miterfrnats@gmail.com>
2022-05-17 22:17:49 +0800

    test: try to fix generate firmware error

--- api/Services/FirmwareGenerateService.cs
+++ api/Services/FirmwareGenerateService.cs
@@ -89,6 +89,7 @@ namespace Homo.IotApi
 
                 System.Console.WriteLine($"testing:{Newtonsoft.Json.JsonConvert.SerializeObject($"{staticPath}/archived/{folderName}", Newtonsoft.Json.Formatting.Indented)}");
                 System.Console.WriteLine($"testing:{Newtonsoft.Json.JsonConvert.SerializeObject(zipSourcePath, Newtonsoft.Json.Formatting.Indented)}");
+                System.IO.Directory.CreateDirectory($"{staticPath}/archived/{folderName}");
                 System.IO.Directory.Move(zipSourcePath, $"{staticPath}/archived/{folderName}");
 
                 // countdown 10 mins then remove zip file

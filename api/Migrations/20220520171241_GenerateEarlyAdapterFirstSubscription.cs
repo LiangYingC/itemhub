5970d69e0e2e0d35e594f9b05b49604115e201e7
Peter Huang <miterfrnats@gmail.com>
2022-05-21 01:31:32 +0800

    feat: 實作 first subscription for early adapter

---
+++ api/Migrations/20220520171241_GenerateEarlyAdapterFirstSubscription.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IotApi.Migrations
+{
+    public partial class GenerateEarlyAdapterFirstSubscription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE `User` Set IsEarlyBird = 1");
+            migrationBuilder.Sql(@"INSERT INTO `Subscription` (CreatedAt, OwnerId, StartAt, EndAt, PricingPlan, TransactionId) 
+            SELECT NOW(), Id, NOW(), LAST_DAY(NOW()), 1, 0 FROM `User`");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE `User` Set IsEarlyBird = 0");
+            migrationBuilder.Sql(@"DELETE FROM `Subscription` WHERE TransactionId = 0 AND CreatedAt <= '2022-06-11'");
+        }
+    }
+}

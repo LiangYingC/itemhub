56a163b0c0366227c9eb93f5a8a0c6969c4b596a
Peter Huang <miterfrnats@gmail.com>
2022-05-21 19:19:32 +0800

    fix: 修正 migration 第一次訂閱結束時間

--- api/Migrations/20220520171241_GenerateEarlyAdapterFirstSubscription.cs
+++ api/Migrations/20220520171241_GenerateEarlyAdapterFirstSubscription.cs
@@ -8,7 +8,7 @@ namespace IotApi.Migrations
         {
             migrationBuilder.Sql("UPDATE `User` Set IsEarlyBird = 1 WHERE CreatedAt <= 2022-06-11");
             migrationBuilder.Sql(@"INSERT INTO `Subscription` (CreatedAt, OwnerId, StartAt, EndAt, PricingPlan, TransactionId) 
-            SELECT NOW(), Id, NOW(), LAST_DAY(NOW()), 1, 0 FROM `User`");
+            SELECT NOW(), Id, NOW(), DATE_ADD(DATE_ADD(DATE_ADD(LAST_DAY(NOW()), INTERVAL 23 HOUR), INTERVAL 59 MINUTE), INTERVAL 59 SECOND), 1, 0 FROM `User`");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
